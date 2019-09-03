0cf43e259c50ab86c618a84cd627bdd246fea2ce
Justin Miller <jayman1294@gmail.com>
2019-09-03 03:42:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
      :railroads => {
        :rent_in_dollars => {
          :one_piece_owned => 25,
-         :nm => 2,
+         : => 2,
          :mn => 3,
          :jk => 4
          },
